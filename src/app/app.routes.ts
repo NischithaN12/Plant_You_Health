5f6c8f8d0b2359194b9f695c4114d3d406d6ab33
- <jeevitha.s@siemens.com>
2025-09-08 14:45:40 +0530

    Updated code with latest changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login_Signup } from './Login_Signup/ls';
 
-export const routes: Routes = [];
+import { App } from './app';
+import { ProfileSetupComponent } from './ProfileSetup/ps';
+
+export const routes = [
+  {path: '', component: App },
+   
+  {path: 'login', component: Login_Signup },
+  {path: 'profile', component: ProfileSetupComponent},
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject, signal, ViewChild } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatStepper, MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+    imports: [
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    RouterOutlet,
+    
+],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
   protected readonly title = signal('Frontend');
+
+  isLinear = true;
+
+
+  constructor(private router: Router) {}
+
+  goToLogin() {
+                   // move stepper to Step 2
+    this.router.navigate(['/login']); // load LoginComponent inside router-outlet
+  }
+  goToProfile() {
+                   // move stepper to Step 3
+    this.router.navigate(['/profile']); // load ProfileSetupComponent inside router-outlet
+  }
 }

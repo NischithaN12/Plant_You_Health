7a3b498cd230aa2fbe83bfe69ff1aa33788dbfed
Nischitha N <nischitha12.blr@gmail.com>
2025-09-08 16:21:11 +0530

    Initial commit for Plant_You_Health Angular project

---
+++ src/app/Profile_setup/ps.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+/**
+ * @title Toolbar overview
+ */
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'toolbar-overview-example',
+  templateUrl: 'ps.html',
+  styleUrl: 'ps.css',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
+})
+
+
+export class ProfileSetupComponent {
+ 
+ 
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
-import { Login_Signup } from './Login_Signup/ls';
+
 
 import { App } from './app';
-import { ProfileSetupComponent } from './ProfileSetup/ps';
+import { ProfileSetupComponent } from './Profile_setup/ps';
 
 export const routes = [
   {path: '', component: App },
-   
-  {path: 'login', component: Login_Signup },
+
   {path: 'profile', component: ProfileSetupComponent},
 
 ];

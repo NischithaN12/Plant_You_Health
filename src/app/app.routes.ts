3b7b9c30ac5fb234afaca6cc429bf35432af8272
- <jeevitha.s@siemens.com>
2025-09-08 16:40:52 +0530

    WIP: my local changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { ProfileSetupComponent } from './ProfileSetup/ps';
 export const routes = [
   {path: '', component: App },
    
-  {path: 'login', component: Login_Signup },
+  { path: 'login', component: Login_Signup },
   {path: 'profile', component: ProfileSetupComponent},
 
 ];
